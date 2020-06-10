e6397b29f58b0f0a019833344033a9838340493e
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-09 20:12:34 -0400

    completed comments table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :comments
   resources :posts
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
