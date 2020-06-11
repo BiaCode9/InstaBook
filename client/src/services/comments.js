54dea1479a417e9e48df16afb3196eb87af8ae47
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-11 15:13:27 -0400

    error fix with staging

--- client/src/services/comments.js
+++ client/src/services/comments.js
@@ -1,9 +1,28 @@
 import api from './api-helper';
 
-// make a function that will make the api call
-// have one route for posts?
+// make a functions that will make the api call
 
 export const getAllComments = async () => {
   const resp = await api.get('/comments');
   return resp.data;
+}
+
+export const getOneComment = async (id) => {
+  const resp = await api.get(`/comments/${id}`);
+  return resp.data;
+}
+
+export const createComment = async (commentData) => {
+  const resp = await api.post('/comments', { comment: commentData });
+  return resp.data;
+}
+
+export const updateComment = async (id, commentData) => {
+  const resp = await api.put(`/foods/${id}`, { comment: commentData });
+  return resp.data;
+}
+
+export const deleteComment = async (id) => {
+  const resp = await api.delete(`/comments/${id}`);
+  return resp
 }
