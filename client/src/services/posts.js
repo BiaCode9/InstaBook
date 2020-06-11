b3e1238042bcb270e0b63f4d0731ef015c90cf02
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-11 15:22:14 -0400

    created functions for read

---
+++ client/src/services/posts.js
@@ -0,0 +1,28 @@
+import api from './api-helper';
+
+// make a functions that will make the api call
+
+export const getAllPosts = async () => {
+  const resp = await api.get('/posts');
+  return resp.data;
+}
+
+export const getOnePost = async (id) => {
+  const resp = await api.get(`/posts/${id}`);
+  return resp.data;
+}
+
+export const createComment = async (commentData) => {
+  const resp = await api.post('/comments', { comment: commentData });
+  return resp.data;
+}
+
+export const updateComment = async (id, commentData) => {
+  const resp = await api.put(`/foods/${id}`, { comment: commentData });
+  return resp.data;
+}
+
+export const deleteComment = async (id) => {
+  const resp = await api.delete(`/comments/${id}`);
+  return resp
+}
