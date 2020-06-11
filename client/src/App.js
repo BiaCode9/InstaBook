f190a2579e0a2d3f3438393b99332d0660df1ef3
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-11 19:35:34 -0400

    added handlelogin to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,10 @@ export default class App extends Component {
     this.setState({ user });
   }
 
-
+  handleLoginSubmit = async (data) => {
+    const user = await loginUser(data);
+    this.setState({ user });
+  }
 
 
 
