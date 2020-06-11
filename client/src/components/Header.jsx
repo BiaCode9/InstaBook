faa55ddab8bcefb937900e891ffcc75c152deb82
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-11 17:05:23 -0400

    header created with log in link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div>
-
+      <h1>InstaBook</h1>
+      <Link to='/user/login'>Log In</Link>
     </div>
   )
 }
