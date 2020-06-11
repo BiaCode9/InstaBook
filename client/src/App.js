89aa5575f7e94e54c8c4e08a0038e8484cbfc0f9
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-11 17:32:25 -0400

    added import Main in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
+import Main from './components/Main'
 
 export default class App extends Component {
   render() {
