df9f9c5192d8e7244921bf53d983fb54f7101f1f
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-12 21:26:50 -0400

    got background image for log-in and other fixes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -16,49 +16,56 @@ export default class Login extends Component {
   }
 
   render() {
+
     const { username, password } = this.state;
     const { handleLoginSubmit, history } = this.props;
     return (
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        handleLoginSubmit(this.state);
-        history.push('/');
-        this.setState({
-          username: "",
-          password: ""
-        })
-      }}>
-        <div className='form-container-login'>
-          <div className="login-title">
-            <h3>Welcome Back!</h3>
-          </div>
-          <label htmlFor="username">User Name:
+      <body>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          handleLoginSubmit(this.state);
+          history.push('/');
+          this.setState({
+            username: "",
+            password: ""
+          })
+        }}>
+          <div className='form-container-login'>
+            <div className="login-title">
+              <h3>Welcome Back!</h3>
+            </div>
+            <label htmlFor="username">User Name:
           <input
-              id="username"
-              type="text"
-              name="username"
-              value={username}
-              placeholder="JonnyDoe"
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <label htmlFor="password">Password:
+                id="username"
+                type="text"
+                name="username"
+                value={username}
+                placeholder="JonnyDoe"
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <label htmlFor="password">Password:
           <input
-              id="password"
-              type="password"
-              name="password"
-              value={password}
-              placeholder="123456"
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <div className="login-button">
-            <button>Login to InstaBook </button>
+                id="password"
+                type="password"
+                name="password"
+                value={password}
+                placeholder="123456"
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <div className="login-button">
+              <button>Login to InstaBook </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </body>
+
+
     )
+
   }
+
 }
