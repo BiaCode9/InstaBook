be247ea16ad94be93aa62e599b39d164604644b7
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-14 19:24:02 -0400

    updated user posts container css

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './CreatePost.css'
 
 export default class CreatePost extends Component {
   state =
@@ -24,88 +25,81 @@ export default class CreatePost extends Component {
     const { user_id, name, location, description, startdate, enddate } = this.state;
     const { postPost, history } = this.props;
     return (
-      <div className="create-container">
-        <form onSubmit={(e) => {
+      <form className="create-container" onSubmit={(e) => {
 
-          e.preventDefault();
-          postPost(this.state);
-          history.push('/posts');
-          this.setState({
-            user_id: "",
-            name: "",
-            location: "",
-            description: "",
-            startdate: "",
-            enddate: ""
-          })
-        }}>
+        e.preventDefault();
+        postPost(this.state);
+        history.push('/posts');
+        this.setState({
+          user_id: "",
+          name: "",
+          location: "",
+          description: "",
+          startdate: "",
+          enddate: ""
+        })
+      }}>
 
-          <div className="post-show-container">
+        <hr />
 
-            <hr />
+        <h3>Create Post</h3>
 
+        <label htmlFor="title">User:</label>
+        <input
+          id="id"
+          type="text"
+          value={user_id}
+          name='user_id'
+          onChange={this.handleChange}
+        />
 
-            <h3>Create Post</h3>
-
-            <label htmlFor="title">User:</label>
-            <input
-              id="id"
-              type="text"
-              value={user_id}
-              name='user_id'
-              onChange={this.handleChange}
-            />
-
-            <label htmlFor="title">Job Title:</label>
-            <input
-              id="id"
-              type="text"
-              value={name}
-              name='name'
-              onChange={this.handleChange}
-            />
-            <label htmlFor="title">Location:</label>
-            <input
-              id="id"
-              type="text"
-              value={location}
-              name='location'
-              onChange={this.handleChange}
-            />
-            <label htmlFor="title">Start Date:</label>
-            <input
-              id="id"
-              type="text"
-              value={startdate}
-              name='startdate'
-              onChange={this.handleChange}
-            />
-            <label htmlFor="title">End Date:</label>
-            <input
-              id="id"
-              type="text"
-              value={enddate}
-              name='enddate'
-              onChange={this.handleChange}
-            />
+        <label htmlFor="title">Job Title:</label>
+        <input
+          id="id"
+          type="text"
+          value={name}
+          name='name'
+          onChange={this.handleChange}
+        />
+        <label htmlFor="title">Location:</label>
+        <input
+          id="id"
+          type="text"
+          value={location}
+          name='location'
+          onChange={this.handleChange}
+        />
+        <label htmlFor="title">Start Date:</label>
+        <input
+          id="id"
+          type="text"
+          value={startdate}
+          name='startdate'
+          onChange={this.handleChange}
+        />
+        <label htmlFor="title">End Date:</label>
+        <input
+          id="id"
+          type="text"
+          value={enddate}
+          name='enddate'
+          onChange={this.handleChange}
+        />
 
 
-            <label htmlFor="post">Post:</label>
-            <textarea
-              className="textarea-description"
-              rows={10}
-              placeholder='Description'
-              value={description}
-              name='description'
-              required
-              onChange={this.handleChange}
-            />
-            <button>Submit</button>
-          </div>
-        </form>
-      </div>
+        <label htmlFor="post">Post:</label>
+        <textarea
+          className="textarea-description"
+          rows={10}
+          placeholder='Description'
+          value={description}
+          name='description'
+          required
+          onChange={this.handleChange}
+        />
+        <button className="submit-button">Submit</button>
+      </form>
 
     )
-
   }
 }
