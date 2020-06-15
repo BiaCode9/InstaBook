017e06b6fd2288c937cdbc9a024304c003eed68d
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-15 19:26:20 -0400

    updated comment user

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import './Comment.css'
 
 export default function Comment({ comment }) {
   return (
     <p>
-      {comment.description}
-      {comment.user.username}
+      <div className="comments">
+        <div className="user-comment">
+          {comment.user.username}
+        </div>
+        <div className="comment-description">
+          {comment.description}
+        </div>
+      </div>
+
     </p>
+
+
+
+    //   <button className="delete-button" onClick={() => destroyPost(post.id)}>Delete</button>
   )
 }
