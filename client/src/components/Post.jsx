ebce29449967c87fb3376c5e4c8ca88b25e43b2f
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-15 01:04:40 -0400

    fixed css with single post container

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -26,55 +26,62 @@ class Post extends PureComponent {
     } = this.props
     return (
 
+
+
       <React.Fragment key={post.id}>
 
-        <p><div className="user"></div><div className="user-entry">
-          {post.user.username}</div></p>
+        <div className="single-post-container">
+
+          <p><div className="user"></div><div className="user-entry">
+            {post.user.username}</div></p>
 
 
-        <p><div className="position">POSITION:</div><div className="job-entry">{post.name}</div></p>
+          <p><div className="position">POSITION:</div><div className="job-entry">{post.name}</div></p>
 
 
-        <p><div className="location">LOCATION:</div><div className="location-entry">{post.location}</div></p>
+          <p><div className="location">LOCATION:</div><div className="location-entry">{post.location}</div></p>
 
 
-        <p><div className="start-date">START DATE:</div><div className="start-date-entry">{post.startdate}</div></p>
+          <p><div className="start-date">START DATE:</div><div className="start-date-entry">{post.startdate}</div></p>
 
 
-        <p><div className="end-date">END DATE:</div><div className="end-date-entry">{post.enddate}</div></p>
+          <p><div className="end-date">END DATE:</div><div className="end-date-entry">{post.enddate}</div></p>
 
 
-        <p><div className="description">DESCRIPTION:</div><div className="description-entry">{post.description}</div></p>
+          <p><div className="description">DESCRIPTION:</div><div className="description-entry">{post.description}</div></p>
 
-        {
-          currentUser && currentUser.id === post.user_id && (
-            <>
-              <div className="edit-delete">
-                <button className="edit-button">{currentUser && <Link to='/edit/post/'>Edit</Link>}</button>
-                <button className="delete-button" onClick={() => destroyPost(post.id)}>Delete</button>
-              </div>
-            </>
-          )
-        }
-
-
-
+          {
+            currentUser && currentUser.id === post.user_id && (
+              <>
+                <div className="edit-delete">
+                  <button className="edit-button">{currentUser && <Link to='/edit/post/'>Edit</Link>}</button>
+                  <button className="delete-button" onClick={() => destroyPost(post.id)}>Delete</button>
+                </div>
+              </>
+            )
+          }
 
 
 
-        {post.comments.map(comment => (
-          <p>{comment.description}</p>
-        ))}
-        <div className="comment-box">
-          <textarea name="comment" value={this.state.comment} placeholder="write your comment here"
-            onChange={this.handleChange}
-          />
+
+
+
+          {post.comments.map(comment => (
+            <p>{comment.description}</p>
+          ))}
+          <div className="comment-box">
+            <textarea name="comment" value={this.state.comment} placeholder="write your comment here"
+              onChange={this.handleChange}
+            />
+          </div>
         </div>
       </React.Fragment>
-      // </div>
+
 
 
     )
+
+
   }
 }
 
